bee95bcb2fc32689b4dc948c5abedd86635876b2
DotNetCurry <dotnetcurry@a2zknowledgevisuals.com>
2014-06-26 07:34:31 +0530

    Xamarin-files

---
+++ DNC.NewsReader/DNC.NewsReader/App.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+using Shared;
+
+namespace DNC.NewsReader
+{
+public class HackerNewsPage : ContentPage
+{
+    private ListView listView;
+    public HackerNewsPage()
+    {
+        Title = "Hacker News Stories";
+
+        listView = new ListView
+        {
+            RowHeight = 80
+        };
+
+        Content = new StackLayout
+        {
+            VerticalOptions = LayoutOptions.FillAndExpand,
+            Children = { listView }
+        };
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        var entries = await new HackerNewsRepository().TopEntriesAsync();
+        listView.ItemTemplate = new DataTemplate(typeof(HackerNewsEntryCell));
+        listView.ItemsSource = entries;
+    }
+}
+public class HackerNewsEntryCell : ViewCell
+{
+    public HackerNewsEntryCell()
+    { 
+        var title = new Label();
+        title.SetBinding(Label.TextProperty, "Title");
+            
+        var postedBy = new Label();
+        postedBy.SetBinding(Label.TextProperty, "PostedBy");
+
+        View = new StackLayout
+        {
+            Children = { title, postedBy }
+        };
+    }
+
+    protected override void OnTapped()
+    {
+        base.OnTapped();
+
+        var entry = BindingContext as Shared.Entry;
+
+        var article = new WebView
+        {
+            Source = new UrlWebViewSource
+            {
+                Url = entry.Url,
+            },
+            VerticalOptions = LayoutOptions.FillAndExpand
+        };
+
+        App.Navigation.PushAsync(new ContentPage { Title = entry.Title, Content = article });
+    }
+}
+public class App
+{
+    public static INavigation Navigation { get; private set; }
+	public static Xamarin.Forms.Page GetMainPage()
+	{
+		var navigationPage = new NavigationPage(new HackerNewsPage());
+        Navigation = navigationPage.Navigation;
+
+        return navigationPage;
+	}
+}
+}
---
+++ Shared/HackerNewsRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Shared
+{
+    public class HackerNewsRepository
+    {
+        public async Task<IEnumerable<Entry>> TopEntriesAsync()
+        {
+            var client = new HttpClient();
+
+            var result = await client.GetStringAsync("http://api.ihackernews.com/page/");
+
+            return JsonConvert.DeserializeObject<Page>(result).Items;
+        }
+    }
+
+    public class Entry
+    {
+        public int Id { get; set; }
+        public int Points { get; set; }
+        public string PostedAgo { get; set; }
+        public string PostedBy { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+    public class Page
+    {
+        public int? Next { get; set; }
+        public IEnumerable<Entry> Items { get; set; }
+    }
+}
